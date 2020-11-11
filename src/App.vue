657cb725aab3ce72be626ec6af7ac21e5ec36824
edidada <sandisks555@gmail.com>
2020-11-11 14:13:56 +0800

    add some

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <MySelect msg="Welcome to Your Vue.js App"/>
+    <MySelect />
   </div>
 </template>
 
--- src/components/MySelect.vue
+++ src/components/MySelect.vue
@@ -3,7 +3,7 @@
   <div class="hello">
     <select v-model="selectItem" @change="selectFn($event)">
       <!--选择项的value值默认选择项文本 可动态绑定选择项的value值 更改v-model指令绑定数据-->
-      <option v-for="item in items" v-bind:key="item.id" :value="item.id">{{item.name}}</option>
+      <option v-for="item in items" v-bind:key="item.id" :value="item.name">{{item.name}}</option>
     </select>
     <div>{{selectItem}}</div>    <!--选择项的value值-->
   </div>
@@ -32,7 +32,7 @@ new Vue({
 export default {
   name: 'MySelect',
   props: {
-    msg: [
+    items: [
       {id:11, name: '分类1'},
       {id:22, name: '分类2'},
       {id:33, name: '分类3'}
